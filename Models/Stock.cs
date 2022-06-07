5b5152dd5b8387e44ff5352e69c4aa1bd2ffe758
Matheus Piovani <matheuspiovani@gmail.com>
2022-06-07 20:06:06 -0300

    Change labels to ptBR

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -1,7 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace broker.Models
 {
+    [DisplayName("Ação")]
     public class Stock
     {
         public int Id { get; set; }
@@ -9,11 +11,14 @@ namespace broker.Models
         [Required]
         public string Ticker { get; set; } = null!;
 
+        [DisplayName("Último Preço")]
         public decimal LastPrice { get; set; }
 
         [Required]
+        [DisplayName("Moeda")]
         public string Currency { get; set; } = null!;
 
+        [DisplayName("Alertas")]
         public List<Alert>? Alerts { get; set; }
 
         public Stock()
